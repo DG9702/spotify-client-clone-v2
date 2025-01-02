5e4a919c97a8209886db158aec5d123be0bff3c8
Haibt9702 <buihaibq9702@gmail.com>
2025-01-02 16:16:12 +0700

    first commit

---
+++ src/constants/spotify.ts
@@ -0,0 +1,13 @@
+export const MADE_FOR_YOU_URI = '0JQ5DAt0tbjZptfcdMSKl3';
+
+export const INITIAL_VOLUME = 0.2;
+
+export const PLAYLIST_DEFAULT_IMAGE = `${process.env.PUBLIC_URL}/images/playlist.png`;
+
+export const ARTISTS_DEFAULT_IMAGE = `${process.env.PUBLIC_URL}/images/artist.png`;
+
+export const LIKED_SONGS_IMAGE = `${process.env.PUBLIC_URL}/images/liked-songs.png`;
+
+export const EQUILISER_IMAGE = `${process.env.PUBLIC_URL}/images/equaliser-animated.gif`;
+
+export const DEFAULT_PAGE_COLOR = '#121212';
